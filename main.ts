8de36d2e8f03e76ebac6de4d434f7c4fb661b7d3
Garosh <malbios@googlemail.com>
2018-04-09 21:16:10 +0200

    Forgot to change Client import to GameClient

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
-import Client from './client'
+import {GameClient} from './client'
 
 let credentials = require('./credentials.json')
 
 let gameId = '21e3175c-316f-4782-b7b8-731f39b023ef'
 
-let nobotClient = new Client()
-let prismClient = new Client()
+let nobotClient = new GameClient()
+let prismClient = new GameClient()
 
 nobotClient.initWithCredentials(credentials[0].user, credentials[0].pass, gameId)
 prismClient.initWithCredentials(credentials[1].user, credentials[1].pass, gameId)
